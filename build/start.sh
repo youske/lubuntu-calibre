2c3293a6422e494f73a1c4740fdfffc6c2f3924c
youske miyakoshi <youske@gmail.com>
2016-05-27 19:51:46 +0900

    modify

--- build/start.sh
+++ build/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -eu
 
-gosu vnc bash -c "vncserver ${VNC_OPTIONS} && tail -f /dev/null"
+su -c "vncserver -depth ${VNC_DEPTH} -geometry ${VNC_GEOMETRY} -rfbport ${VNC_PORT} :${VNC_DISPLAY}" vnc
+gosu ${VNC_USER} /bin/bash -c "calibre-server --auto-reload --with-library ${CALIBRE_LIBRARY} --max-cover ${VNC_MAXCOVER} --user ${CALIBRE_USER} --password ${CALIBRE_PASSWORD} --port ${CALIBRE_PORT}"
