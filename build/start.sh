09736f6a54f1af53faa4d8b4cc40d55984fc80bc
youske miyakoshi <youske@gmail.com>
2016-06-20 02:05:22 +0900

    modify

--- build/start.sh
+++ build/start.sh
@@ -3,5 +3,5 @@
 echo "vnc password : ${VNC_PASSWORD}"
 echo "calibre password : ${CALIBRE_PASSWORD}"
 
-su -c "vncserver -depth ${VNC_DEPTH} -geometry ${VNC_GEOMETRY} -rfbport ${VNC_PORT} :${VNC_DISPLAY}" vnc
-gosu ${VNC_USER} /bin/bash -c "calibre-server --auto-reload --with-library ${CALIBRE_LIBRARY} --max-cover ${VNC_MAXCOVER} --user ${CALIBRE_USER} --password ${CALIBRE_PASSWORD} --port ${CALIBRE_PORT}"
+#su -c "vncserver -depth ${VNC_DEPTH} -geometry ${VNC_GEOMETRY} -rfbport ${VNC_PORT} :${VNC_DISPLAY}" vnc
+#gosu ${VNC_USER} /bin/bash -c "calibre-server --auto-reload --with-library ${CALIBRE_LIBRARY} --max-cover ${CALIBRE_MAXCOVER} --user ${CALIBRE_USER} --password ${CALIBRE_PASSWORD} --port ${CALIBRE_PORT}"
